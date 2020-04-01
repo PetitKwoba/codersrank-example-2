4c5a20c48ae252185e3a5bdd945ce4e86028172d
PetitKwoba <kwobapetit@gmail.com>
2020-04-01 15:13:44 +0300

    query selectors in js file

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Start with the index.html and start editing.
 
 import './style.css';
+const header = document.querySelector("header");
+const main = document.getElementById('main');
+const section = document.querySelectorAll('section');
+
